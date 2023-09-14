6710b34a553f2e682d8389a4b92de2b173c99954
HalienCoder <nihaal.sivapuram@gmail.com>
2023-09-15 00:15:52 +0530

    minor changes

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("btnTop");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function goUp() {
+  document.documentElement.scrollTop = 0;
+}
